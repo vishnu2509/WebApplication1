bca6d4759087a247463eddac9cc484928af3971d
vishnu2509 <107933278+vishnu2509@users.noreply.github.com>
2022-06-26 17:13:39 +0530

    Add files via upload

---
+++ OfficeContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeePRO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EmployeePRO
+{
+    public class OfficeContext : DbContext
+    {
+        public OfficeContext() : base("dbconn")
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
